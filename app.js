efbf633a33137cc78fc3b854978e73e8d1c5648f
OJHA Ayush <ayojha@emea.msad.sopra>
2017-05-17 12:05:33 +0530

    buttons displayed, basic flow without db

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// This loads the environment variables from the .env file
-//require('dotenv-extended').load();
-
 var restify = require('restify');
 var builder = require('botbuilder');
 var Store = require('./store');
@@ -25,29 +22,6 @@ var customers = [
     }
 ];
 
-// var products = [
-//     {product_id : 1,name : 'LORRAINE KELLY SCUBA FIT & FLARE DRESS',quantity : 10,1,70,NULL},
-//     {product_id : 2,name : 'BLACK TROPICAL PRINT SHIRRED BODYCON DRESS',quantity : 10,1,35,NULL},
-//     {product_id : 3,name : 'PLEATED PLEAT SKIRT MAXI DRESS',quantity : 10,1,65,NULL},
-//     {product_id : 4,name : 'JOANNA HOPE GYPSY MAXI DRESS',quantity : 10,1,45,NULL},
-//     {product_id : 5,name : 'JOANNA HOPE GYPSY MAXI DRESS',quantity : 10,1,45,NULL},
-//     {product_id : 6,name : 'BELL SLEEVE LACE DRESS',quantity : 10,1,75,NULL},
-//     {product_id : 7,name : 'BELL SLEEVE LACE DRESS',quantity : 10,1,75,NULL},
-//     {product_id : 8,name : 'BLACK/RED FLORAL SPLIT SLEEVE MAXI DRESS',quantity : 10,1,45,NULL},
-//     {product_id : 9,name : 'LORRAINE KELLY BURNOUT FIT & FLARE',quantity : 10,1,65,NULL},
-//     {product_id : 10,name : 'BLACK SHORT SLEEVE V NECK BARDOT TOP',quantity : 10,2,15,NULL},
-//     {product_id : 11,name : 'PLEAT CAMISOLE',quantity : 10,2,10,NULL},
-//     {product_id : 12,name : 'PLEAT CAMISOLE',quantity : 10,2,10,NULL},
-//     {product_id : 13,name : 'PLEAT CAMISOLE',quantity : 10,2,10,NULL},
-//     {product_id : 14,name : 'COLD SHOULDER GYPSY TOP',quantity : 10,2,20,NULL},
-//     {product_id : 15,name : 'COLD SHOULDER GYPSY TOP',quantity : 10,2,20,NULL},
-//     {product_id : 16,name : 'PRINT GYPSY JERSEY TOP',quantity : 10,2,20,NULL},
-//     {product_id : 17,name : 'DROP SLEEVE SHELL TOP',10,2,16,NULL},
-//     {product_id : 18,name : 'PREMIUM SHAPE AND SCULPT MID BLUE HIGH WAISTED BOO',10,5,40,NULL},
-//     {product_id : 19,name : 'NIGHTINGALES TIERED DRESS',10,1,59,NULL},
-//     {product_id : 20,name : 'LORRAINE KELLY FLORAL CHIFFON SKIRT',10,9,55,NULL},
-//     {product_id : 21,name : 'DOBBY SLEEVE WRAP SHIRT',10,7,22,NULL}
-// ]
 
 //=========================================================
 // Bot Setup
@@ -107,11 +81,7 @@ bot.dialog('returnItem', [
 
     },
     function (session, results, next) {
-        // if (results.response[0] != null) {
-        //     var items = results.response[0];
-        //     if (results.response.length == 2)
-        //         var date = results.response[1];
-        // }
+
         var items = results.response[0];
         var date = results.response[1];
         // Async search
@@ -120,14 +90,11 @@ bot.dialog('returnItem', [
             .then(function (listOfItems) {
                 // args
                 session.send('I found %d items:', listOfItems.length);
-
                 var message = new builder.Message()
                     .attachmentLayout(builder.AttachmentLayout.carousel)
                     .attachments(listOfItems.map(itemAsAttachment));
 
                 session.send(message);
-
-                // while (selectedItem != null);
                 session.send(selectedItem);
                 next();
                 // session.endDialog();
@@ -157,66 +124,15 @@ bot.dialog('returnItem', [
     }
 });
 
-// bot.dialog('itemSelected', [
-//     function (session, args, next) {
-//         // // Get color and optional size from users utterance
-//         // //var utterance = args.intent.matched[0];
-//         // //var color = /(white|gray)/i.exec(utterance);
-//         // //var size = /\b(Extra Large|Large|Medium|Small)\b/i.exec(utterance);
-//         // //if (color) {
-//         //     // Initialize cart item
-//         //     var item = session.dialogData.item = { 
-//         //         product: "classic " + color[0].toLowerCase() + " t-shirt",
-//         //         size: size ? size[0].toLowerCase() : null,
-//         //         price: 25.0,
-//         //         qty: 1
-//         //     };
-//         //     if (!item.size) {
-//         //         // Prompt for size
-//         //         builder.Prompts.choice(session, "What size would you like?", "Small|Medium|Large|Extra Large");
-//         //     } else {
-//         //         //Skip to next waterfall step
-//         //         next();
-//         //     }
-//         // } else {
-//         //     // Invalid product
-//         //     session.send("I'm sorry... That product wasn't found.").endDialog();
-//         // }   
-//         session.send("You have made a selection");
-//     }//,
-//     // function (session, results) {
-//     //     // Save size if prompted
-//     //     var item = session.dialogData.item;
-//     //     if (results.response) {
-//     //         item.size = results.response.entity.toLowerCase();
-//     //     }
-
-//     //     // Add to cart
-//     //     if (!session.userData.cart) {
-//     //         session.userData.cart = [];
-//     //     }
-//     //     session.userData.cart.push(item);
-
-//     //     // Send confirmation to users
-//     //     session.send("A '%(size)s %(product)s' has been added to your cart.", item).endDialog();
-//     // }
-// ]).triggerAction({ matches: 'wehjbfkfkjadsgfakjsnfjs' });
 // Helpers
 function itemAsAttachment(item) {
+    // console.log("session"+ session);
+    // console.log("item:"+ item);
     return new builder.HeroCard()
         .title(item.name)
-        .subtitle('COLOUR : %s', item.color)
-        .images([new builder.CardImage().url(item.image)]);
-        // .buttons([
-        //     builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
-        // ]);
+        // .subtitle('COLOUR : %s',item)
+        .images([new builder.CardImage().url(item.image)])
+        .buttons([new builder.CardAction().title('Select').type('openUrl').value('http://google.com')
+        ]);
 }
 // // Add help dialog
-// bot.dialog('help', function (session) {
-//     session.send("I'm a simple echo bot.");
-// }).triggerAction({
-//     matches: /^help/i
-// });
-// function getData(item) {
-//     selectedItem = 'You selected ' + item.name;
-// }
