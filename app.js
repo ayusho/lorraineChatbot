4c14a5ea9d8db3149ca5db678cf38b66a5679132
OJHA Ayush <ayojha@emea.msad.sopra>
2017-05-26 11:40:08 +0530

    product name hard coded and some text changes

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var customerListJSON;
 //=========================================================
 var customer = [
     {
-        customer_id: ''
-        , name: ''
-        , email: ''
-        , phone: ''
-        , address: ''
+        customer_id: '',
+        name: '',
+        email: '',
+        phone: '',
+        address: ''
     }
 ];
 var orderData = [];
@@ -30,8 +30,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: 'c4d12a93-c875-47ca-9700-28e949ec657a'
-    , appPassword: 'spZVMeScmRcN7QdP3afw5wE'
+    appId: 'c4d12a93-c875-47ca-9700-28e949ec657a',
+    appPassword: 'spZVMeScmRcN7QdP3afw5wE'
 });
 server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
@@ -54,22 +54,20 @@ bot.dialog('returnItem', [
                 if (args.intent.entities[i].resolution.values[0].type == 'date') {
                     startDate = args.intent.entities[i].resolution.values[0].value;
                     endDate = null;
-                }
-                else /*if (args.intent.entities[i].resolution.values[0].type == 'daterange') */ {
+                } else /*if (args.intent.entities[i].resolution.values[0].type == 'daterange') */ {
                     console.log("inside daterange");
                     startDate = args.intent.entities[i].resolution.values[0].start;
                     endDate = args.intent.entities[i].resolution.values[0].end;
                 }
-            }
-            else if (args.intent.entities[i].type == 'items') itemType = args.intent.entities[i].resolution.values[0];
+            } else if (args.intent.entities[i].type == 'items') itemType = args.intent.entities[i].resolution.values[0];
         }
         console.log("start date : " + startDate);
         console.log("end date : " + endDate);
         console.log("item : " + itemType);
         var options = {
-            host: 'lorrainewebservice.azurewebsites.net'
-            , path: '/api/getCustomerList'
-            , method: 'GET'
+            host: 'lorrainewebservice.azurewebsites.net',
+            path: '/api/getCustomerList',
+            method: 'GET'
         };
         var req = http.request(options, function (res) {
             console.log('STATUS: ' + res.statusCode);
@@ -105,8 +103,9 @@ bot.dialog('returnItem', [
     }
 
 
-    
-    , function (session, results, next) {
+
+    ,
+    function (session, results, next) {
         var startDate = results.response[0];
         var endDate = results.response[1];
         var itemType = results.response[2];
@@ -115,8 +114,7 @@ bot.dialog('returnItem', [
             // args
             if (startDate == null || startDate == undefined || startDate == '') {
                 session.send('I found %d items you bought:', listOfItems.length);
-            }
-            else {
+            } else {
                 session.send('I found %d items you bought on %s:', listOfItems.length, startDate);
             }
             var message = new builder.Message().attachmentLayout(builder.AttachmentLayout.carousel).attachments(listOfItems.map(function (item) {
@@ -133,8 +131,8 @@ bot.dialog('returnItem', [
     },
 
 ]).triggerAction({
-    matches: 'returnItem'
-    , onInterrupted: function (session) {
+    matches: 'returnItem',
+    onInterrupted: function (session) {
         session.send('Please provide information');
     }
 });
@@ -144,8 +142,9 @@ bot.dialog('/returnReason', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         console.log(results.response);
         session.userData.returnReason = results.response;
         session.send("Thanks for your response.");
@@ -153,8 +152,8 @@ bot.dialog('/returnReason', [
             session.beginDialog('/returnMethod');
         }, 1000);
     }]).triggerAction({
-    matches: /^You selected.*/
-    , onInterrupted: function (session) {
+    matches: /^You selected.*/,
+    onInterrupted: function (session) {
         session.send('Please provide information');
     }
 });
@@ -166,8 +165,9 @@ bot.dialog('/returnMethod', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.returnMethod = results.response.entity;
         session.send('Okay. The nearest Post Office to your delivery address is:');
         session.send('Broadway Post Office\n\n1 Broadway,\n\nWestminster,\n\nLondon SW1H 0AX');
@@ -194,8 +194,9 @@ bot.dialog('/endReturn', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.yesOrNo = results.response.entity;
         if (session.userData.yesOrNo == 'No') {
             session.send('Okay thanks Alison, goodbye');
@@ -215,9 +216,9 @@ bot.dialog('orderItem', [
                 if (res[1] == 'skirt') size = 14;
                 else if (res[1] == 'blouse') size = 12;
                 orderData.push({
-                    itemColor: res[0]
-                    , itemName: res[1]
-                    , itemSize: size
+                    itemColor: res[0],
+                    itemName: res[1],
+                    itemSize: size
                 })
             }
         }
@@ -233,8 +234,8 @@ bot.dialog('orderItem', [
 
 
 ]).triggerAction({
-    matches: 'orderItem'
-    , onInterrupted: function (session) {
+    matches: 'orderItem',
+    onInterrupted: function (session) {
         session.send('Please provide information');
     }
 });
@@ -263,8 +264,7 @@ bot.dialog('/orderSizeInput', [
         if (orderData[counter].itemSize == null) {
             builder.Prompts.number(session, 'What size of ' + orderData[counter].itemColor + ' ' + orderData[counter].itemName + ' would you like to order?');
             console.log("after prompt");
-        }
-        else {
+        } else {
             next({
                 response: null
             });
@@ -272,8 +272,9 @@ bot.dialog('/orderSizeInput', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         console.log("orderSizeInput function 2" + results.response);
         if (results.response != null) orderData[counter].itemSize = results.response;
         //session.userData.selectedItems = [];
@@ -296,13 +297,12 @@ bot.dialog('/afterItemSelected', [
         if (counter < orderData.length) {
             session.send('That\'s lovely, great choice');
             session.beginDialog('/orderSizeInput');
-        }
-        else session.beginDialog('/afterItemOrdered');
+        } else session.beginDialog('/afterItemOrdered');
         //session.endDialog();
     }
 ]).triggerAction({
-    matches: /Added.*/
-    , onInterrupted: function (session) {
+    matches: /Added.*/,
+    onInterrupted: function (session) {
         session.send('Please provide information');
     }
 });
@@ -314,8 +314,9 @@ bot.dialog('/afterItemOrdered', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.afterItemOrderedyesOrNo = results.response.entity;
         console.log("afterItemOrdered " + results.response.entity);
         if (session.userData.afterItemOrderedyesOrNo == 'No') {
@@ -332,8 +333,9 @@ bot.dialog('/orderDeliveryAddress', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.orderDeliveryAddressResponse = results.response.entity;
         console.log("orderDeliveryAddress " + results.response.entity);
         if (session.userData.orderDeliveryAddressResponse == 'Yes') {
@@ -350,8 +352,9 @@ bot.dialog('/deliveryType', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.yesOrNo = results.response.entity;
         if (session.userData.yesOrNo == 'Next Day') {
             session.beginDialog('/paymentType');
@@ -367,8 +370,9 @@ bot.dialog('/paymentType', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.yesOrNo = results.response.entity;
         if (session.userData.yesOrNo == 'Yes') {
             session.beginDialog('/confirmDelivery');
@@ -378,14 +382,15 @@ bot.dialog('/paymentType', [
 ]);
 bot.dialog('/confirmDelivery', [
     function (session) {
-        builder.Prompts.choice(session, 'Perfect. Can you confirm that you would like to purchase X product on your account today.', ['Yes', 'No'], {
+        builder.Prompts.choice(session, 'Perfect. Can you confirm that you would like to purchase Wrap Front Pencil Skirt Size 14 and Piped Blouse Size 12 on your account today?', ['Yes', 'No'], {
             listStyle: builder.ListStyle.button
         });
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.yesOrNo = results.response.entity;
         if (session.userData.yesOrNo == 'Yes') {
             session.beginDialog('/endOrder');
@@ -402,8 +407,9 @@ bot.dialog('/endOrder', [
     }
 
 
-    
-    , function (session, results) {
+
+    ,
+    function (session, results) {
         session.userData.yesOrNo = results.response.entity;
         if (session.userData.yesOrNo == 'No') {
             orderData = [];
@@ -418,9 +424,9 @@ bot.send('/deleteprofile');*/
 function getCustomerData() {
     return new Promise(function (resolve) {
         var options = {
-            host: 'lorrainewebservice.azurewebsites.net'
-            , path: '/api/getCustomerList'
-            , method: 'GET'
+            host: 'lorrainewebservice.azurewebsites.net',
+            path: '/api/getCustomerList',
+            method: 'GET'
         };
         var req = http.request(options, function (res) {
             console.log('STATUS: ' + res.statusCode);
